120fc597d8f08b5dea4b208d5c6f33c1ab31b580
Hansik Hwang <sksmsgkstlr@naver.com>
2020-07-30 22:29:25 +0900

    MainActivity completation

--- app/src/main/java/com/example/onbid/MainActivity.kt
+++ app/src/main/java/com/example/onbid/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.onbid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (rotate.getAnimation() == null) {
+            val rotateAnimation : Animation = AnimationUtils.loadAnimation (getApplicationContext(), R.anim.anim_image)
+            rotate.startAnimation(rotateAnimation)
+            rotate.setAnimation(rotateAnimation)
+        }
+        Handler().postDelayed({
+            startActivity(Intent(this, Home::class.java))
+
+        },4500)
     }
 }
