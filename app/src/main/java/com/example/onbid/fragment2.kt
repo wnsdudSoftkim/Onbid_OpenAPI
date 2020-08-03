91414cdf37715563bfa8076365aceebe48ac6dc1
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2020-08-03 17:50:39 +0900

    Search UI(Tab2)

--- app/src/main/java/com/example/onbid/fragment2.kt
+++ app/src/main/java/com/example/onbid/fragment2.kt
@@ -1,11 +1,14 @@
 package com.example.onbid
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_fragment2.*
 
 class fragment2 : Fragment() {
     override fun onCreateView(
@@ -15,4 +18,26 @@ class fragment2 : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.activity_fragment2,container,false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        text_search_how.setOnClickListener {
+            startActivityForResult(Intent(context,fragment2_searchpopup::class.java),200)
+        }
+
+    }
+    //돌아오는 값 받기
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==200) {
+            if(resultCode==Activity.RESULT_OK) {
+                val result1 = data?.getStringExtra("check1")
+                val result2 = data?.getStringExtra("check2")
+                text_search_result1.setText(result1)
+                text_search_result2.setText(result2)
+            }
+        }
+    }
 }
+
+
---
+++ app/src/main/java/com/example/onbid/fragment2_searchpopup.kt
@@ -0,0 +1,76 @@
+package com.example.onbid
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.activity_fragment2_searchpopup.*
+
+class fragment2_searchpopup : AppCompatActivity() {
+    private var check1 :String?="전체"
+    private var check2 :String?="전체"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment2_searchpopup)
+        //뒤로가기
+        btn_popupclose.setOnClickListener {
+            onBackPressed()
+        }
+        btn_popupsave.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("check1",check1)
+            intent.putExtra("check2",check2)
+            setResult(Activity.RESULT_OK,intent)
+            finish()
+
+        }
+
+
+    }
+    //라디오 체크 시 텍스트 값을 넘겨줌
+    fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+            when (view.id) {
+                R.id.how_total ->
+                    if(checked) {
+                        check1=how_total.text.toString()
+
+                    }
+                R.id.how_sell->
+                    if(checked) {
+                        check1=how_sell.text.toString()
+                    }
+                R.id.how_borrow ->
+                    if(checked) {
+                        check1=how_borrow.text.toString()
+                    }
+
+                R.id.thing_total->
+                    if(checked){
+                        check2 = thing_total.text.toString()
+                    }
+                R.id.thing_car->
+                    if(checked){
+                        check2 = thing_car.text.toString()
+                    }
+
+                R.id.thing_thing->
+                    if(checked){
+                        check2 = thing_thing.text.toString()
+                    }
+
+                R.id.thing_money->
+                    if(checked){
+                        check2 = thing_money.text.toString()
+                    }
+
+
+            }
+        }
+    }
+
+}
